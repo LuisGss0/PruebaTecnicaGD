8d2f07a9b9cb4d2b4e4a443339bfc66346f09751
LuisGss0 <luisanze30@gmail.com>
2025-01-27 10:59:35 -0600

    Websockets

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
-// filepath: /c:/xampp/htdocs/prueba-tecnica-gd/vite.config.js
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    // Cargar variables de entorno
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        plugins: [
-            laravel({
-                input: 'resources/js/app.jsx',
-                refresh: true,
-            }),
-            react(),
-        ],
-        define: {
-            'process.env': env,
-        },
-    };
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
