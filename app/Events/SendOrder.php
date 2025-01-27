e7c739ebebdd597910cb3f9f456f79511880b0d0
LuisGss0 <luisanze30@gmail.com>
2025-01-27 11:09:16 -0600

    Websockets

--- app/Events/SendOrder.php
+++ app/Events/SendOrder.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SendOrder implements ShouldBroadcastNow
+class SendOrder implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
