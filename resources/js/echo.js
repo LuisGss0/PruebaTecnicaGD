3521409263b8791aa0edbbcd75a2b26c2abff38c
LuisGss0 <luisanze30@gmail.com>
2025-01-27 00:03:49 -0600

    Websockets

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,4 +3,19 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
  
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+});
 
+//Mostrar en consola el estatus de la conexión
+window.Echo.connector.pusher.connection.bind('connected', () => {
+    console.log('Conectado a Pusher');
+});
+
+//Mostrar en consola el estatus de la conexión
+window.Echo.connector.pusher.connection.bind('disconnected', () => {
+    console.log('Desconectado de Pusher');
+});
